a5f333c3a5a23f99068a67d406114f1d77b14250
salehahmad798 <saleh315555@gmail.com>
2024-06-11 13:22:26 +0500

    send code

--- lib/screens/slpash/slpash_screen.dart
+++ lib/screens/slpash/slpash_screen.dart
@@ -7,7 +7,6 @@ import 'package:career_center/utils/app_images.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 
 
 
